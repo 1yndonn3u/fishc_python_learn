821d41f6085907fb75b31e116486341a41735a90
meilimin_dxm <meilimin@duxiaoman.com>
2018-11-18 22:40:18 +0800

    lesson2

---
+++ lesson2/cal.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+tmp = input("请输入1到100之间的数字:")
+number = int(tmp)
+if 1 < number < 100:
+    print("你妹好漂亮。")
+else:
+    print("你大爷好丑。")
---
+++ lesson2/hello.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+name = input("请输入您的姓名：")
+hello = "你好，" + name + "!"
+print(hello)
